e1dfd16ab9a7e29ed4a8a9519674e1fb29befe04
edenavrahami <eden.avrahami@mail.huji.ac.il>
2025-01-14 15:53:14 +0200

    created the movement of charlie and the lion

---
+++ Assets/Scripts/CharlieMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CharlieMovement : MonoBehaviour
+{
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int IsGrounded = Animator.StringToHash("IsGrounded");
+    [SerializeField] private LionMovement lionMovement;
+    [SerializeField] private Animator animator;
+
+    void Start()
+    {
+        FixedJoint2D joint = gameObject.AddComponent<FixedJoint2D>();
+        joint.connectedBody = lionMovement.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        SyncAnimations();
+    }
+
+    private void SyncAnimations()
+    {
+        // סנכרון מהירות האנימציה עם האריה
+        animator.SetFloat(Speed, lionMovement.GetComponent<Animator>().GetFloat(Speed));
+        animator.SetBool(IsGrounded, lionMovement.GetComponent<Animator>().GetBool(IsGrounded));
+    }
+    
+}
